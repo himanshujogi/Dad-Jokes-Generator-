b9a64ea3aaa0a0b82f8f8ca3e554a74f88b6e6be
Himanshu Jogi <jogihimanshu2020@gmail.com>
2023-05-29 16:00:55 +0530

    rest files

---
+++ app.js
@@ -0,0 +1,33 @@
+// API => https://icanhazdadjoke.com/ 
+
+const jokes = document.getElementById('jokes')
+const btn = document.getElementById('jokesbtn')
+
+
+btn.addEventListener("click", getjokes);
+// done with Async & Await
+async function getjokes() {
+    const config = {
+        headers: {
+            Accept: "application/json"
+        }
+    };
+    const response = await fetch('https://icanhazdadjoke.com/', config);
+    // console.log(res);
+    const data = await response.json();
+    // console.log(data);
+    jokes.innerHTML = data.joke;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
